ca6292354dd629c6272d7332dceb05d1f264484d
ZHANGBAIFA9 <1350862999@qq.com>
2021-01-13 13:24:59 +0800

    /ZHANGBAIFA

---
+++ myjava/src/main/java/com/myjava/algorithm/leetcode/first100/TwoNumSumLian.java
@@ -0,0 +1,38 @@
+package com.myjava.algorithm.leetcode.first100;
+
+/**
+ * @Version 1.0
+ * @Author ZHANGBAIFA
+ * @Date 2021/1/13 13:21
+ * @Description:
+ *      给你两个 非空 的链表，表示两个非负的整数。它们每位数
+ *      字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ *
+ *      请你将两个数相加，并以相同形式返回一个表示和的链表。
+ *      你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *          示例1：
+ *          输入：l1 = [2,4,3], l2 = [5,6,4]
+ *          输出：[7,0,8]
+ *          解释：342 + 465 = 807.
+ *
+ *          示例2：
+ *          输入：l1 = [0], l2 = [0]
+ *          输出：[0]
+ *
+ *          示例3：
+ *          输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ *          输出：[8,9,9,9,0,0,0,1]
+ *
+ */
+public class TwoNumSumLian {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] answer(int[] src1 , int[] src2){
+
+        return null ;
+    }
+
+
+}
