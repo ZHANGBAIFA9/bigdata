1db81cc45db23f60c6be171ede84a1fd86ab5f68
ZHANGBAIFA9 <1350862999@qq.com>
2021-02-26 13:49:15 +0800

    flume 不同日志类型写入不同topic、日志数据etl /ZHANGBAIFA

---
+++ myflume/src/main/java/com/myflume/source/LogTypeInterceptor.java
@@ -0,0 +1,57 @@
+package com.myflume.source;
+
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.interceptor.Interceptor;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Version 1.0
+ * @Author ZHANGBAIFA
+ * @Date 2021/2/26 13:41
+ * @Description:
+ */
+public class LogTypeInterceptor implements Interceptor {
+    public void initialize() {
+
+    }
+
+    public Event intercept(Event event) {
+        // 区分日志类型：   body  header
+        // 1 获取body数据
+        byte[] body = event.getBody();
+        String log = new String(body, Charset.forName("UTF-8"));
+        // 2 获取header
+        Map<String, String> headers = event.getHeaders();
+        // 3 判断数据类型并向Header中赋值
+        if (log.contains("start")) {
+            headers.put("topic","topic_start");
+        }else {
+            headers.put("topic","topic_event");
+        }
+        return event;
+    }
+
+    public List<Event> intercept(List<Event> events) {
+        ArrayList<Event> interceptors = new ArrayList<>();
+        for (Event event : events) {
+            Event intercept1 = intercept(event);
+            interceptors.add(intercept1);
+        }
+        return interceptors;
+    }
+
+    public void close() {}
+    public static class Builder implements  Interceptor.Builder{
+        @Override
+        public Interceptor build() {
+            return new LogTypeInterceptor();
+        }
+        @Override
+        public void configure(Context context) {
+        }
+    }
+}
