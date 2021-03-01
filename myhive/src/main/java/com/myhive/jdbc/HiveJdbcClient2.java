7d6a15207ca3d6ab9b24a55c5d1dbacae06de743
ZHANGBAIFA9 <1350862999@qq.com>
2021-03-01 20:21:50 +0800

    新增hive模块 /ZHANGBAIFA

--- myhive/src/main/java/com/myhive/jdbc/HiveJdbcClient2.java
+++ myhive/src/main/java/com/myhive/jdbc/HiveJdbcClient2.java
@@ -2,6 +2,8 @@ package com.myhive.jdbc;
 
 import java.sql.SQLException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Version 1.0
@@ -19,11 +21,22 @@ public class HiveJdbcClient2 {
         }
         Connection conn = DriverManager.getConnection("jdbc:hive2://node2,node3,node4/bigdata;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2_zk", "root", "");
         Statement stmt = conn.createStatement();
-        String sql = "select * from person";
+        String sql = "select * from t";
         ResultSet res = stmt.executeQuery(sql);
+        List<String> list = new ArrayList<String>();
+        int i = 0 ;
         while (res.next()) {
-            System.out.println(res.getString(1)+"-" + res.getString("name"));
+//            System.out.println(res.getString(1)+"-" + res.getString("name"));
+            list.add(res.getString(2));
+            i++ ;
+            System.out.print(res.getString(1)+"\t");
+            if( null != res.getString(2)){
+                System.out.println(list.get(i-1));
+            }else{
+                System.out.println(res.getString(2));
+            }
         }
+        
     }
 
 }
