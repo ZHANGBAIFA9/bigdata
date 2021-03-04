f46caa374330bb177f0f0de5db433045596d6fd9
ZHANGBAIFA9 <1350862999@qq.com>
2021-03-04 20:17:52 +0800

    dwd层日志模型创建及数据加载脚本 /ZHANGBAIFA

---
+++ myhive/src/main/java/com/myhive/indicators/dwd/ods_to_dwd_base_log.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# ods数据加载到dwd层脚本
+# 定义变量方便修改
+APP=gmall
+hive=/opt/module/hive/bin/hive
+
+# 如果是输入的日期按照取输入日期；如果没输入日期取当前时间的前一天
+if [ -n "$1" ] ;then
+	do_date=$1
+else
+	do_date=`date -d "-1 day" +%F`
+fi
+
+sql="
+use gmall;
+insert overwrite table ${APP}.dwd_base_event_log partition(dt='$do_date')
+select
+    ${APP}.base_analizer(line,'mid') as mid_id,
+    ${APP}.base_analizer(line,'uid') as user_id,
+    ${APP}.base_analizer(line,'vc') as version_code,
+    ${APP}.base_analizer(line,'vn') as version_name,
+    ${APP}.base_analizer(line,'l') as lang,
+    ${APP}.base_analizer(line,'sr') as source,
+    ${APP}.base_analizer(line,'os') as os,
+    ${APP}.base_analizer(line,'ar') as area,
+    ${APP}.base_analizer(line,'md') as model,
+    ${APP}.base_analizer(line,'ba') as brand,
+    ${APP}.base_analizer(line,'sv') as sdk_version,
+    ${APP}.base_analizer(line,'g') as gmail,
+    ${APP}.base_analizer(line,'hw') as height_width,
+    ${APP}.base_analizer(line,'t') as app_time,
+    ${APP}.base_analizer(line,'nw') as network,
+    ${APP}.base_analizer(line,'ln') as lng,
+    ${APP}.base_analizer(line,'la') as lat,
+    event_name,
+    event_json,
+    ${APP}.base_analizer(line,'st') as server_time
+from ${APP}.ods_event_log lateral view ${APP}.flat_analizer(${APP}.base_analizer(line,'et')) tem_flat as event_name,event_json
+where dt='$do_date'  and ${APP}.base_analizer(line,'et')<>'';
+"
+
+$hive -e "$sql";
